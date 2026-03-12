b601c07ea9ecfb4379dcc7e5f032a1814ce1a28a
Daniil <daniilvolk1900@gmail.com>
2026-03-12 19:41:19 +0300

    hw1 done

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,45 @@
+﻿while (true)
+{
+    Console.WriteLine("Enter two numbers (or 'q' to exit)");
+    string input = Console.ReadLine();
+
+    if (input == "q")
+    {
+        break;
+    }
+
+    string[] numbers = input.Split(' ');
+
+    if (numbers.Length != 2 || !double.TryParse(numbers[0], out double num1) || !double.TryParse(numbers[1], out double num2))
+    {
+        Console.WriteLine("Invalid input");
+        continue;
+    }
+
+    Console.WriteLine("Choose operation (+, -, *, /)");
+    string operation = Console.ReadLine();
+
+    switch (operation)
+    {
+        case "+":
+            Console.WriteLine(num1 + num2);
+            break;
+        case "-":
+            Console.WriteLine(num1 - num2);
+            break;
+        case "*":
+            Console.WriteLine(num1 * num2);
+            break;
+        case "/":
+            if (num2 == 0)
+                Console.WriteLine("Error: Division by zero");
+            else
+                Console.WriteLine(num1 / num2);
+            break;
+        default:
+            Console.WriteLine("Invalid operation");
+            break;
+    }
+
+    Console.WriteLine();
+}
